a44010c95035b16233038e4259e4abf6ccab9f33
MANORANJITHAM12 <49307824+MANORANJITHAM12@users.noreply.github.com>
2019-04-22 09:24:05 +0530

    positive.c

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+print("enter the number\n");
+scanf("%d",&num);
+if(num>0)
+{
+printf("number is positive");
+}
+else
+{
+printf("number is negative")
+}
+return 0;
+}
