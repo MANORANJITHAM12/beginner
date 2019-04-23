4aa2b4db01f01dc6b20e06338d8c00f43b064b6f
MANORANJITHAM12 <49307824+MANORANJITHAM12@users.noreply.github.com>
2019-04-23 14:35:28 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+int input;
+printf("enter the input\n");
+scanf("%c",&input);
+if((input>='A' && input >='Z')\\(input>='a' && input<='z'))
+{
+printf("alphabet");
+}
+else
+{
+printf("No");
+}
+return 0;
+}
