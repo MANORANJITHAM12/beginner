3df146cf605cd7d6782acb0d2717a107f18c92f6
MANORANJITHAM12 <49307824+MANORANJITHAM12@users.noreply.github.com>
2019-04-23 14:10:33 +0530

    odd_even.c

---
+++ odd_even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+printf("enter the number\n");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("given number is even");
+}
+else
+{
+printf("given numberis odd");
+}
+return 0;
+}
