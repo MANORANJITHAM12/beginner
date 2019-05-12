26914fb38045e17e63dc8f85d947127b6d9c53ec
MANORANJITHAM12 <49307824+MANORANJITHAM12@users.noreply.github.com>
2019-05-12 14:34:47 +0530

    lastn_char.c

---
+++ lastn_char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[20];
+int n,length,diiff,i;
+scanf("%s%d",&str,&n);
+length=strlen(str);
+diff=length-n;
+for(i=diff;i<length;i++)
+{
+printf("%s",str[i]);
+}
+return 0;
+}
