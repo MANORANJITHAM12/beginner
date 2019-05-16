9bf0d35ac26637282e42e3274122cc9333bdf3e2
MANORANJITHAM12 <49307824+MANORANJITHAM12@users.noreply.github.com>
2019-05-16 16:29:14 +0530

    kthodd.py

---
+++ kthodd.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number"))
+k=int(input("enter the number"))
+lst=[]
+count=0
+for i in range(0,n):
+m=int(input())
+lst.append(m)
+for j in lst:
+if j%2!=0:
+count=count+1
+if count==kprint(j)
